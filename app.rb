d9c91c1ec593703602d2abd257a78116b6bc970b
barita <44899916+angarita-dev@users.noreply.github.com>
2020-11-09 18:04:53 -0500

    Adding call to WebStatsHandler

--- app.rb
+++ app.rb
@@ -1,6 +1,7 @@
 require 'zeitwerk'
 
 loader = Zeitwerk::Loader.new
+loader.push_dir('./app/')
 loader.push_dir('./app/models')
 loader.push_dir('./app/services')
 loader.push_dir("#{__dir__}/lib/api_faker")
@@ -11,5 +12,10 @@ def setup
 end
 
 def call
+  setup
+
+  web_stats_handler = WebStatsHandler.new
+  web_stats_handler.parse_stats
+  web_stats_handler.save_stats
 end
 
