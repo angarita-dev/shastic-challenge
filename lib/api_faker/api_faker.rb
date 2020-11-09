c48c50ef9fa544c8201517bf54ea58686ea2f29e
barita <44899916+angarita-dev@users.noreply.github.com>
2020-11-09 17:22:38 -0500

    Fixed ApiFaker

--- lib/api_faker/api_faker.rb
+++ lib/api_faker/api_faker.rb
@@ -1,13 +1,17 @@
-require 'webmock'
-                               
-include WebMock::API           
+class ApiFaker
+  require 'webmock'
+  include WebMock::API           
 
-WebMock.enable!                
+  WebMock.enable!                
 
-# Mocking requests             
-stub_request(                  
-  :get,
-  'www.shastic-challenge.fake-data.com'
-).to_return(
-  body: File.read(File.expand_path('../public/sample.json', __FILE__))
-) 
+  def setup_stub
+    # Mocking requests             
+    stub_request(                  
+                 :get,
+                 'www.shastic-challenge.fake-data.com'
+                ).to_return(
+                  body: File.read(File.expand_path('../public/sample.json', __FILE__))
+                ) 
+                true
+  end
+end
